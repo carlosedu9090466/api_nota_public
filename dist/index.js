a9b0db8b89f7ba723835849389c6399e06e2801f
carlos eduardo <carlosedu9090466@gmail.com>
2023-01-03 08:14:28 -0400

    sa

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-"use strict";const express = require('express');
+const express = require('express');
 const cors = require('cors');
 const router = require('./routes/routes');
 const app = express();
